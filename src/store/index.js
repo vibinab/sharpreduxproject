f6ca81a83853c0f3622a460a9967566d8b451b0c
vibinab <abvibin4@gmail.com>
2022-08-03 11:27:25 +0530

    counter

--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,13 @@ import { createStore } from 'redux'
 const counterReducer=(state={ counter:0},action)=>{
 if(action.type==='increment'){
     return {
-        counter:state.counter+1
+        counter:state.counter+5
     }
 }
 
 if(action.type==='decrement'){
     return {
-        counter:state.counter-1
+        counter:state.counter-5
     }
 }
 
